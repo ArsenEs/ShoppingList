69a18573d005e93c9e397f52ebab45b6dc96883d
ArsenEs <arsenesenov12@gmail.com>
2023-12-13 22:37:09 +0600

    lessson1

--- app/src/main/java/com/srenes/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/srenes/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.srenes.shoppinglist
+package com.srenes.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.srenes.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
